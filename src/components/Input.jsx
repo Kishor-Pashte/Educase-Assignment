05aa7ac538119c228d03c0c047d9c2d9f243e34b
Amit Kumar <akumar6199@gmail.com>
2025-08-24 00:07:52 +0530

    login button correction

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,20 @@
 const style = "border border-light-gray rounded-md w-full h-[40px] px-[15px] focus:outline-none";
 
-export function Input({ label, type, placeholder, required }) {
+export function Input({ label, type, placeholder, required, onChange }) {
   return (
     <div className="relative mt-6">
       <label htmlFor={label} className="text-blue text-[13px] absolute top-[-10px] left-[9px] bg-body pl-1 pr-2">
         {label}
         {required && <span className="text-warning">*</span>}
       </label>
-      <input type={type} id={label} placeholder={placeholder} className={style} required={required} />
+      <input
+        type={type}
+        onChange={onChange}
+        id={label}
+        placeholder={placeholder}
+        className={style}
+        required={required}
+      />
     </div>
   )
 }
