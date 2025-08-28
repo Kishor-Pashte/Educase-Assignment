1f3f740d7e33663b1d73959818b4507a7c04ac50
KISHOR PASHTE <k24145798@gmail.com>
2025-08-28 17:10:10 +0530

    changes updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,115 @@
-import { Heading, Button } from "../../components";
-import { Link } from "react-router";
+import { Button } from "../../components/Button";
+import { Heading } from "../../components/Heading";
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
     <div className="w-full h-full flex p-5">
       <div className="mt-auto">
+        <div
+          data-html2canvas-ignore="true"
+          className="_2nWnjq7kj-M6XmfWMyI_f- ccx-annotation"
+          data-annotationid="/assets/urn:aaid:sc:US:b68eea25-003d-4a5d-8fdd-d463eeb20b32/annots/36d3b7de-0f8f-4f1c-900c-293c793b4178"
+          data-annotationtype="pin"
+          style={{
+            borderRadius: "100%",
+            height: "28px",
+            lineHeight: "34px",
+            left: "420.315px",
+            top: "154.2043px",
+            width: "28px",
+            position: "absolute",
+            backgroundColor: "#E7B000",
+          }}
+        >
+          <span
+            style={{
+              color: "white",
+              position: "absolute",
+              left: "10px",
+              bottom: "-4px",
+            }}
+          >
+            1
+          </span>
+        </div>
+        <div
+          data-html2canvas-ignore="true"
+          className="_2nWnjq7kj-M6XmfWMyI_f- ccx-annotation"
+          data-annotationid="/assets/urn:aaid:sc:US:b68eea25-003d-4a5d-8fdd-d463eeb20b32/annots/36d3b7de-0f8f-4f1c-900c-293c793b4178"
+          data-annotationtype="pin"
+          style={{
+            borderRadius: "100%",
+            height: "28px",
+            lineHeight: "34px",
+            left: "400.315px",
+            top: "205.2043px",
+            width: "28px",
+            position: "absolute",
+            backgroundColor: "#E7B000",
+          }}
+        >
+          <span
+            style={{
+              color: "white",
+              position: "absolute",
+              left: "10px",
+              bottom: "-4px",
+            }}
+          >
+            2
+          </span>
+        </div>
+
+        <div
+          data-html2canvas-ignore="true"
+          className="_2nWnjq7kj-M6XmfWMyI_f- ccx-annotation"
+          data-annotationid="/assets/urn:aaid:sc:US:b68eea25-003d-4a5d-8fdd-d463eeb20b32/annots/36d3b7de-0f8f-4f1c-900c-293c793b4178"
+          data-annotationtype="pin"
+          style={{
+            borderRadius: "100%",
+            height: "28px",
+            lineHeight: "34px",
+            left: "410.315px",
+            top: "240.2043px",
+            width: "28px",
+            position: "absolute",
+            backgroundColor: "#E7B000",
+          }}
+        >
+          <span
+            style={{
+              color: "white",
+              position: "absolute",
+              left: "10px",
+              bottom: "-4px",
+            }}
+          >
+            3
+          </span>
+        </div>
+
         <Heading content="Welcome to PopX" />
-        <p className="text-primary opacity-[.6] pr-[85px] mb-7 text-[18px] leading-[26px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p className="text-primary opacity-[.6] pr-[85px] mb-7 text-[18px] leading-[26px]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
         <Link to="/signup">
-          <Button name={"Create Account"} type="button" category={"primary"} disabled={false} />
+          <Button
+            name={"Create Account"}
+            type="button"
+            category={"primary"}
+            disabled={false}
+          />
         </Link>
         <Link to="/login">
-          <Button name={"Already Registered? Login"} type="button" category={"secondary"} disabled={false} />
+          <Button
+            name={"Already Registered? Login"}
+            type="button"
+            category={"secondary"}
+            disabled={false}
+          />
         </Link>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,65 +1,72 @@
-import { Heading, Input, Button } from "../../components";
+import { Heading } from "../../components/Heading";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function Login() {
-    const [disabled, setDisabled] = useState(true);
-    const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function handleEmail(e) {
-        setEmail(e.target.value);
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  useEffect(() => {
+    if (email !== "" && password !== "") {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
     }
-    function handlePassword(e) {
-        setPassword(e.target.value);
-    }
+  }, [email, password]);
 
-    useEffect(() => {
-        if (email !== "" && password !== "") {
-            setDisabled(false);
-        } else {
-            setDisabled(true);
-        }
-    }, [email, password])
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    navigate("/profile");
+  }
 
-        navigate("/profile");
-    }
+  return (
+    <div className="w-full h-full flex p-5">
+      <div>
+        <Heading
+          content="Signin to your PopX account"
+          className="leading-[36px] pr-[100px]"
+        />
+        <p className="text-primary opacity-[.6] pr-[85px] mt-2 text-[18px] leading-[26px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        </p>
+        <form className="text-[13px]" onSubmit={handleSubmit}>
+          <Input
+            label="Email Address"
+            type="email"
+            placeholder="Enter email address"
+            onChange={handleEmail}
+            value={email}
+          />
+          <Input
+            label="Password"
+            type="password"
+            placeholder="Enter password"
+            onChange={handlePassword}
+            value={password}
+          />
 
-    return (
-        <div className="w-full h-full flex p-5">
-            <div>
-                <Heading content="Signin to your PopX account" className="leading-[36px] pr-[100px]" />
-                <p className="text-primary opacity-[.6] pr-[85px] mt-2 text-[18px] leading-[26px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                <form className="text-[13px]" onSubmit={handleSubmit}>
-                    <Input
-                        label="Email Address"
-                        type="email"
-                        placeholder="Enter email address"
-                        onChange={handleEmail}
-                        value={email}
-                    />
-                    <Input
-                        label="Password"
-                        type="password"
-                        placeholder="Enter password"
-                        onChange={handlePassword}
-                        value={password}
-                    />
-
-                    <Button
-                        name={"Login"}
-                        type="submit"
-                        category={"primary"}
-                        disabled={disabled}
-                        className="mt-[14px] text-[16px]"
-                    />
-                </form>
-            </div>
-        </div>
-    )
+          <Button
+            name={"Login"}
+            type="submit"
+            category={"primary"}
+            disabled={disabled}
+            className="mt-[14px] text-[16px]"
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
